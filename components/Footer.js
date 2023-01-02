373119a1c9aba628d23ff8493c5e31abe64a3d69
Richie Mugambi <richiemugambi4@gmail.com>
2023-01-02 20:25:45 +0300

    finished template

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,60 @@
-import React from 'react'
+import styles from '../styles/footer.module.css'
+import { BsTwitter, BsFacebook, BsInstagram } from "react-icons/bs";
+import Image from 'next/image'
+import logo from '../public/assets/logos/comboniHealthProgramme-removebg-preview.png'
+import { HiLocationMarker } from "react-icons/hi";
+
+
+// BsTwitter
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className={styles.footer}>
+            <button className={styles.btn_top} type="submit">Back to top</button>
+            {/* newsletter section */}
+            <div className={styles.newsLetter}>
+                <h4 className={styles.newsLetterTitle}>NewsLetter</h4>
+
+                <div className={styles.textNsubscribe}>
+                    <p>Want to recieve updates about Comboni? Subscribe to us.</p>
+                    <section className={styles.btnNinput}>
+                        <input type="text" className={styles.nws_form_input} placeholder='Your Email' />
+                        <button className={styles.nws_btn} type="submit">Subscribe</button>
+                    </section>
+                </div>
+            </div>
+            {/* links section */}
+            <section className={styles.links_heading}>
+                <div className={styles.links_individual}>
+                    <h4>Quick Links</h4>
+                    <div>
+                        <p>Contact Us</p>
+                        <p>About</p>
+                        <p>Help</p>
+
+                    </div>
+                </div>
+                <div className={styles.links_individual}>
+                    <h4>Comboni Health Programme</h4>
+                    <div>
+                        <p><HiLocationMarker /> Nairobi, Kenya</p>
+                        <p>© 2023 Comboni. All rights reserved. We respect your privacy. By using our site, you agree to our terms. </p>
+                    </div>
+                </div>
+            </section>
+            {/* socials section */}
+            <section className="flex_kidogo">
+                <BsTwitter size={22} color="#ffffff" />
+                <BsFacebook size={22} color="#ffffff" />
+                <BsInstagram size={22} color="#ffffff" />
+            </section>
+            {/* final footer section */}
+            <section className='flex_kidogo'>
+                {/* <Image src={logo}></Image> */}
+                <p className='smallP'>Made by Richie Mugambi</p>
+            </section>
+
+        </div>
     )
 }
 
