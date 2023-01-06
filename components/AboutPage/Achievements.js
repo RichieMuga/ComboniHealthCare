63711b4de78c2ae51f2d6f9a09d3911272f16f1b
Richie Mugambi <richiemugambi4@gmail.com>
2023-01-06 18:03:55 +0300

    added achievements

--- components/AboutPage/Achievements.js
+++ components/AboutPage/Achievements.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import Image from 'next/image'
+import seventeenCert from '../../public/assets/officialFile/cerificates/2017cert.png'
+import nineteenCert from '../../public/assets/officialFile/cerificates/2019Rightcert.png'
+
 
 const Achievements = () => {
     return (
         <section>
             <h1 className='normal-header space2'>Achievements</h1>
+            <div className='containerAchievements'>
+                <div className='cert1'>
+                    <Image width={450} height={500} src={seventeenCert} />
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis, nisi.
+                </div>
+                <div>
+                    <Image width={400} height={400} src={nineteenCert} />
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                </div>
+
+            </div>
         </section>
     )
 }
