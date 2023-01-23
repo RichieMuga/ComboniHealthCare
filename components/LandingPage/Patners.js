41f8aeb8ac454bd6ad68b46d338ebeabf101f88f
Richie Mugambi <richiemugambi4@gmail.com>
2023-01-23 14:38:38 +0300

    added carousel functionality on patners section

--- components/LandingPage/Patners.js
+++ components/LandingPage/Patners.js
@@ -2,17 +2,51 @@ import Image from 'next/image'
 import React from 'react'
 import patnersList from '../../utils/patners'
 import styles from '../../styles/LandingPageCss/landing.module.css'
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 
 
 const Patners = () => {
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 8
+        },
+        desktop4: {
+            breakpoint: { max: 3000, min: 1250 },
+            items: 6
+        },
+        deskto3: {
+            breakpoint: { max: 1250, min: 1024 },
+            items: 5
+        },
+        desktop2: {
+            breakpoint: { max: 1024, min: 870 },
+            items: 4
+        },
+        tablet: {
+            breakpoint: { max: 870, min: 620 },
+            items: 3
+        },
+        tablet2: {
+            breakpoint: { max: 620, min: 380 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 380, min: 0 },
+            items: 2
+        }
+    };
     return (
         <div className='space'>
             <h1 className='normal-header'>Patners</h1>
-            <section className={styles.patner_container}>
+            {/* <Carousel className={styles.patner_container}> */}
+            <Carousel className={styles.carousel} responsive={responsive} slidesToSlide={1} infinite={true} autoPlay={true} autoPlaySpeed={2500}>
                 {patnersList.map((patner) => {
                     return (
-                        <div key={patner.id}>
+                        <div key={patner.id} className={styles.individualPatner}>
                             <div className={styles.patner_column}>
                                 <Image height={100} width={100} src={patner.image} alt={patner.title} />
                                 <p>{patner.title}</p>
@@ -20,7 +54,7 @@ const Patners = () => {
                         </div>
                     )
                 })}
-            </section>
+            </Carousel>
 
         </div>
     )
--- utils/patners.js
+++ utils/patners.js
@@ -16,6 +16,36 @@ const patnersList = [
         image: copticLogo,
         title: `Coptic Hospital`,
     },
+    {
+        id: 3,
+        image: copticLogo,
+        title: `Coptic Hospital`,
+    },
+    {
+        id: 4,
+        image: copticLogo,
+        title: `Coptic Hospital`,
+    },
+    {
+        id: 5,
+        image: copticLogo,
+        title: `Coptic Hospital`,
+    },
+    {
+        id: 6,
+        image: copticLogo,
+        title: `Coptic Hospital`,
+    },
+    {
+        id: 7,
+        image: copticLogo,
+        title: `Coptic Hospital`,
+    },
+    {
+        id: 8,
+        image: copticLogo,
+        title: `Coptic Hospital`,
+    },
 ]
 
 export default patnersList
